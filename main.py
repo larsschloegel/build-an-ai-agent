b2e607f69075b4ded7e221f98a6e56abb658d592
lschloegel <lars.schloegel@gmail.com>
2026-03-12 19:18:21 +0100

    CH1 L5: prompt with user input

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
@@ -5,21 +6,23 @@ from google import genai
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
     client = genai.Client(api_key=api_key)
-    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=user_prompt,
+        contents=args.user_prompt,
     )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
 
-    print("User prompt:", user_prompt)
+    print("User prompt:", args.user_prompt)
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")

9dbf99a23cd0b7c72357cf1667893e758e3f6e32
lschloegel <lars.schloegel@gmail.com>
2026-03-22 11:42:38 +0100

    CH3 L6: tell LLM which functions are available to call

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.call_function import available_functions
+from prompts import system_prompt
+
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
@@ -26,6 +29,9 @@ def generate_content(client, messages, args):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
@@ -34,8 +40,14 @@ def generate_content(client, messages, args):
         print("User prompt:", args.user_prompt)
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+
+    if not response.function_calls:
+        print("Response:")
+        print(response.text)
+        return
+
+    for function_call in response.function_calls:
+        print(f"Calling function: {function_call.name}({function_call.args})")
 
 
 if __name__ == "__main__":

21e64e4713151bf2948a41fd7cff38b7913b0984
lschloegel <lars.schloegel@gmail.com>
2026-03-26 08:33:19 +0100

    CH4 L2: Test Agent

--- prompts.py
+++ prompts.py
@@ -8,5 +8,11 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Execute Python files with optional arguments
 - Write or overwrite files
 
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security.
+
+You are called in a loop, so you'll be able to execute more and more function calls with each message, so just take the next step in your overall plan.
+
+Most of your plans should start by scanning the working directory (`.`) for relevant files and directories. Don't ask me where the code is, go look for it with your list tool.
+
+Execute code (both the tests and the application itself, the tests alone aren't enough) when you're done making modifications to ensure that everything works as expected.
 """
